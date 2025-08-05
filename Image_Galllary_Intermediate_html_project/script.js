2b0d37259fb433ffe12c0261ff4d8d95ed57418d
Prit <thombarepritnilesh@gmail.com>
2025-08-05 20:44:15 +0530

    Cat-alogue #6

--- Image_Galllary_Intermediate_html_project/script.js
+++ Image_Galllary_Intermediate_html_project/script.js
@@ -2,31 +2,65 @@ const modal = document.getElementById('modal');
 const galleryItems = document.querySelectorAll('.gallery-item');
 const modalImage = document.querySelector('.modal-image');
 const closeBtn = document.querySelector('.modal-close');
+const leftArrow = document.querySelector('.arrow.left');
+const rightArrow = document.querySelector('.arrow.right');
 
+let currentIndex = -1
 
 galleryItems.forEach(item => {
     item.addEventListener('click', () => {
         const fullSizeSrc = item.getAttribute('data-fullsize-src');
         modalImage.src = fullSizeSrc;
         modal.style.display = 'flex';
+        currentIndex = index;
     });
 });
 
 
 closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
+    modalImage.src = "";
 });
 
     
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.style.display = 'none';
+        modalImage.src = "";
     }
 });
 
     
 window.addEventListener('keydown', (event) => {
-    if (event.key === "Escape" && modal.style.display === 'flex') {
-        modal.style.display = 'none';
+    if (modal.style.display === 'flex') {
+        if (event.key === "Escape") {
+            modal.style.display = 'none';
+            modalImage.src = "";
+        }
+        if (event.key === "ArrowLeft") showPrev();
+        if (event.key === "ArrowRight") showNext();
     }
 });
+
+function showNext() {
+    if (currentIndex < galleryItems.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    const nextSrc = galleryItems[currentIndex].getAttribute('data-fullsize-src');
+    modalImage.src = nextSrc;
+}
+
+function showPrev() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = galleryItems.length - 1; 
+    }
+    const prevSrc = galleryItems[currentIndex].getAttribute('data-fullsize-src');
+    modalImage.src = prevSrc;
+}
+
+leftArrow.addEventListener('click', showPrev);
+rightArrow.addEventListener('click', showNext);
